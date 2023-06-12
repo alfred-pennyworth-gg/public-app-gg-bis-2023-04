45ff11c971349ca339754ba796ea0c087a54b33c
Alfred Pennyworth <alfred-pennyworth-gg@protonmail.com>
2023-06-12 05:33:35 +0200

    Updating app.py

--- app.py
+++ app.py
@@ -2,13 +2,7 @@
 
 import antigravity
 
-CERT="""-----BEGIN EC PRIVATE KEY-----
-MIGkAgEBBDBVRxJ4pHZdThNSDR7R87V8v+VK+Fg0Ew54SxIIJxrF10x+y/KgnRXU
-xy/2YJs8knigBwYFK4EEACKhZANiAAQsZrCc4+fZ+aEtbgNmFbrKPs+0xNU/RQiE
-ap101nZT3yfMytk64nzrRGep7H4EZlNEmBDYrJxElLpF7KOAOo164mBR9RrCqyU9
-lG4qqeFvL5h7ioS2b2G/50F0FEieqYA=
------END EC PRIVATE KEY-----
-"""
+SEED=d3b3e600-0548-4843-b5b3-be07fb2623af
 
 def main():
     antigravity.fly()
